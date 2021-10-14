7b614436dbedafcdfa5a7ef9c21a218812901bf6
joolmosd <joancamilood@hotmail.com>
2021-10-13 23:56:32 -0500

    Commit with Login V2

--- ProyectoCiclo3.App.Frontend/Pages/Servicio/Edit.cshtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Servicio/Edit.cshtml.cs
@@ -7,36 +7,44 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authorization;
 using ProyectoCiclo3.App.Persistencia.AppRepositorios;
 using ProyectoCiclo3.App.Dominio;
- 
+
 namespace ProyectoCiclo3.App.Frontend.Pages
 {
     [Authorize]
     public class EditServicioModel : PageModel
     {
-       private readonly RepositorioServicio repositorioServicio;
-              [BindProperty]
-              public Servicio Servicio {get;set;}
- 
-        public EditServicioModel(RepositorioServicio repositorioServicio)
-       {
-            this.repositorioServicio=repositorioServicio;
-       }
- 
-        public IActionResult OnGet(int servicioId)
+        private readonly RepositorioServicio repositorioServicio;
+        private readonly RepositorioUsuario repositorioUsuario;
+        private readonly RepositorioEncomienda repositorioEncomienda;
+        public IEnumerable<Usuario> Usuarios { get; set; }
+        public IEnumerable<Encomienda> Encomiendas { get; set; }
+
+        [BindProperty]
+        public Servicio Servicio { get; set; }
+
+        public EditServicioModel(RepositorioServicio repositorioServicio,RepositorioEncomienda repositorioEncomienda, RepositorioUsuario repositorioUsuario)
         {
-                Servicio = repositorioServicio.GetServicioWithId(servicioId);
-                return Page();
- 
+            this.repositorioServicio = repositorioServicio;
+            this.repositorioEncomienda = repositorioEncomienda;
+            this.repositorioUsuario = repositorioUsuario;
         }
 
-        public IActionResult OnPost(){
-            if(!ModelState.IsValid)
+        public IActionResult OnGet(int servicioId)
+        {
+            Servicio = repositorioServicio.GetServicioWithId(servicioId);
+            return Page();
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
             {
                 return Page();
             }
-            if(Servicio.id>0)
+            if (Servicio.id > 0)
             {
-            Servicio = repositorioServicio.Update(Servicio);
+                Servicio = repositorioServicio.Update(Servicio);
             }
             return RedirectToPage("./List");
         }
